aa664a1fbb292737d41acc35b2d296bdbe5224c3
Florian Müller <fmui@apache.org>
2010-02-23 15:13:33 +0000

    added the missing support for AtomPub clients that transmit "/xml", "+xml" or "xhtml" content
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/trunk/opencmis@915373 13f79535-47bb-0310-9956-ffa450edef68

--- opencmis-server/opencmis-server-impl/src/main/java/org/apache/opencmis/server/impl/atompub/AtomEntryParser.java
+++ opencmis-server/opencmis-server-impl/src/main/java/org/apache/opencmis/server/impl/atompub/AtomEntryParser.java
@@ -19,7 +19,9 @@
 package org.apache.opencmis.server.impl.atompub;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +29,9 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.opencmis.commons.PropertyIds;
@@ -257,8 +261,6 @@ public class AtomEntryParser {
       }
     }
 
-    next(parser);
-
     if (type.equals("text") || type.equals("html")) {
       fAtomContentStream.setStream(new ByteArrayInputStream(readText(parser).getBytes("UTF-8")));
     }
@@ -356,25 +358,138 @@ public class AtomEntryParser {
    * Copies a subtree into a stream.
    */
   private InputStream copy(XMLStreamReader parser) throws Exception {
-    // ToDo: implement copy
+    // create a writer
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    XMLStreamWriter writer = XMLOutputFactory.newInstance().createXMLStreamWriter(out);
 
+    writer.writeStartDocument();
+
+    // copy subtree
     int level = 1;
     while (next(parser)) {
       int event = parser.getEventType();
       if (event == XMLStreamReader.START_ELEMENT) {
+        copyStartElement(parser, writer);
         level++;
+      }
+      else if (event == XMLStreamReader.CHARACTERS) {
+        writer.writeCharacters(parser.getText());
+      }
+      else if (event == XMLStreamReader.COMMENT) {
+        writer.writeComment(parser.getText());
+      }
+      else if (event == XMLStreamReader.CDATA) {
+        writer.writeCData(parser.getText());
       }
       else if (event == XMLStreamReader.END_ELEMENT) {
         level--;
         if (level == 0) {
           break;
         }
+        writer.writeEndElement();
+      }
+      else {
+        break;
       }
     }
 
+    writer.writeEndDocument();
+
     next(parser);
 
-    return null;
+    return new ByteArrayInputStream(out.toByteArray());
+  }
+
+  /**
+   * Copies a XML start element.
+   */
+  private void copyStartElement(XMLStreamReader parser, XMLStreamWriter writer) throws Exception {
+    String namespaceUri = parser.getNamespaceURI();
+    String prefix = parser.getPrefix();
+    String localName = parser.getLocalName();
+
+    // write start element
+    if (namespaceUri != null) {
+      if ((prefix == null) || (prefix.length() == 0)) {
+        writer.writeStartElement(localName);
+      }
+      else {
+        writer.writeStartElement(prefix, localName, namespaceUri);
+      }
+    }
+    else {
+      writer.writeStartElement(localName);
+    }
+
+    // set namespaces
+    for (int i = 0; i < parser.getNamespaceCount(); i++) {
+      addNamespace(writer, parser.getNamespacePrefix(i), parser.getNamespaceURI(i));
+    }
+    addNamespaceIfMissing(writer, prefix, namespaceUri);
+
+    // write attributes
+    for (int i = 0; i < parser.getAttributeCount(); i++) {
+      String attrNamespaceUri = parser.getAttributeNamespace(i);
+      String attrPrefix = parser.getAttributePrefix(i);
+      String attrName = parser.getAttributeLocalName(i);
+      String attrValue = parser.getAttributeValue(i);
+
+      if ((attrNamespaceUri == null) || (attrNamespaceUri.trim().length() == 0)) {
+        writer.writeAttribute(attrName, attrValue);
+      }
+      else if ((attrPrefix == null) || (attrPrefix.trim().length() == 0)) {
+        writer.writeAttribute(attrNamespaceUri, attrName, attrValue);
+      }
+      else {
+        addNamespaceIfMissing(writer, attrPrefix, attrNamespaceUri);
+        writer.writeAttribute(attrPrefix, attrNamespaceUri, attrName, attrValue);
+      }
+    }
+  }
+
+  /**
+   * Checks if the given prefix is assigned to the given namespace.
+   */
+  @SuppressWarnings("unchecked")
+  private void addNamespaceIfMissing(XMLStreamWriter writer, String prefix, String namespaceUri)
+      throws Exception {
+    if ((namespaceUri == null) || (namespaceUri.trim().length() == 0)) {
+      return;
+    }
+
+    if (prefix == null) {
+      prefix = "";
+    }
+
+    Iterator<String> iter = (Iterator<String>) writer.getNamespaceContext().getPrefixes(
+        namespaceUri);
+    if (iter == null) {
+      return;
+    }
+
+    while (iter.hasNext()) {
+      String p = iter.next();
+      if ((p != null) && (p.equals(prefix))) {
+        return;
+      }
+    }
+
+    addNamespace(writer, prefix, namespaceUri);
+  }
+
+  /**
+   * Adds a namespace to a XML element.
+   */
+  private void addNamespace(XMLStreamWriter writer, String prefix, String namespaceUri)
+      throws Exception {
+    if ((prefix == null) || (prefix.trim().length() == 0)) {
+      writer.setDefaultNamespace(namespaceUri);
+      writer.writeDefaultNamespace(namespaceUri);
+    }
+    else {
+      writer.setPrefix(prefix, namespaceUri);
+      writer.writeNamespace(prefix, namespaceUri);
+    }
   }
 
   /**
