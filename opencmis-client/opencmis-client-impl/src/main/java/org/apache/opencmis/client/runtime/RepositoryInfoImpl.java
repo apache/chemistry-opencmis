100339a457072213daddeba567672e60ffa6cad7
Florian Müller <fmui@apache.org>
2010-02-25 12:10:19 +0000

    - corrected RepositoryInfoImpl
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/trunk/opencmis@916260 13f79535-47bb-0310-9956-ffa450edef68

--- opencmis-client/opencmis-client-impl/src/main/java/org/apache/opencmis/client/runtime/RepositoryInfoImpl.java
+++ opencmis-client/opencmis-client-impl/src/main/java/org/apache/opencmis/client/runtime/RepositoryInfoImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.opencmis.client.runtime;
 
 import java.io.Serializable;
@@ -89,7 +107,6 @@ public class RepositoryInfoImpl implements RepositoryInfo, Serializable {
 
 	public String getProductName() {
 		return this.riData.getProductName();
-
 	}
 
 	public String getProductVersion() {
@@ -101,12 +118,11 @@ public class RepositoryInfoImpl implements RepositoryInfo, Serializable {
 	}
 
 	public String getThinClientUri() {
-		// TODO Auto-generated method stub
-		return null;
+	  return this.riData.getThinClientUri();
 	}
 
 	public String getVendorName() {
-		return this.riData.getThinClientUri();
+		return this.riData.getVendorName();
 	}
 
 }
