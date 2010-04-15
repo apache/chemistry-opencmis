e4c1c35da55e1dd1b387bbb2852fe7475738211d
Florent Guillaume <fguillaume@apache.org>
2010-04-15 14:12:32 +0000

    Move SessionFactoryImpl to toplevel client package
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/opencmis/trunk@934419 13f79535-47bb-0310-9956-ffa450edef68

--- chemistry-opencmis-client/chemistry-opencmis-client-impl/src/main/java/org/apache/chemistry/opencmis/client/SessionFactoryImpl.java
+++ chemistry-opencmis-client/chemistry-opencmis-client-impl/src/main/java/org/apache/chemistry/opencmis/client/SessionFactoryImpl.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.chemistry.opencmis.client.runtime;
+package org.apache.chemistry.opencmis.client;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +25,8 @@ import java.util.Map;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.client.api.SessionFactory;
 import org.apache.chemistry.opencmis.client.api.repository.Repository;
+import org.apache.chemistry.opencmis.client.runtime.CmisBindingHelper;
+import org.apache.chemistry.opencmis.client.runtime.PersistentSessionImpl;
 import org.apache.chemistry.opencmis.client.runtime.repository.RepositoryImpl;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
 import org.apache.chemistry.opencmis.commons.bindings.CmisBinding;
