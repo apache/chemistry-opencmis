630fed6b1b63bb1a2c53150fc45f50a93932c1e2
Florian Müller <fmui@apache.org>
2012-07-22 16:57:06 +0000

    enhanced TCK tests
    git-svn-id: https://svn.apache.org/repos/asf/chemistry/opencmis/trunk@1364357 13f79535-47bb-0310-9956-ffa450edef68

--- chemistry-opencmis-test/chemistry-opencmis-test-tck/src/main/java/org/apache/chemistry/opencmis/tck/tests/crud/UpdateSmokeTest.java
+++ chemistry-opencmis-test/chemistry-opencmis-test-tck/src/main/java/org/apache/chemistry/opencmis/tck/tests/crud/UpdateSmokeTest.java
@@ -41,8 +41,10 @@ import org.apache.chemistry.opencmis.tck.impl.AbstractSessionTest;
  */
 public class UpdateSmokeTest extends AbstractSessionTest {
 
-    private static final String NAME1 = "updatetest1.txt";
-    private static final String NAME2 = "updatetest2.txt";
+    private static final String DOC_NAME1 = "updatetest1.txt";
+    private static final String DOC_NAME2 = "updatetest2.txt";
+    private static final String FOLDER_NAME1 = "updatetest1";
+    private static final String FOLDER_NAME2 = "updatetest2";
 
     @Override
     public void init(Map<String, String> parameters) {
@@ -56,66 +58,102 @@ public class UpdateSmokeTest extends AbstractSessionTest {
         CmisTestResult f;
 
         try {
-            // create folders
+            // create test folder
             Folder testFolder = createTestFolder(session);
 
-            // create document
-            Document doc1 = createDocument(session, testFolder, NAME1, "rename me!");
-            Document workDoc = doc1;
+            // document test
+            updateDocument(session, testFolder);
 
-            f = createResult(FAILURE, "Document name doesn't match with given name!");
-            addResult(assertEquals(NAME1, doc1.getName(), null, f));
-
-            // test if check out is required
-            boolean checkedout = false;
-            DocumentTypeDefinition type = (DocumentTypeDefinition) doc1.getType();
-            PropertyDefinition<?> namePropDef = type.getPropertyDefinitions().get(PropertyIds.NAME);
-            if (namePropDef.getUpdatability() == Updatability.WHENCHECKEDOUT
-                    || (!doc1.getAllowableActions().getAllowableActions().contains(Action.CAN_UPDATE_PROPERTIES) && Boolean.TRUE
-                            .equals(type.isVersionable()))) {
-                workDoc = (Document) session.getObject(doc1.checkOut(), SELECT_ALL_NO_CACHE_OC);
-                checkedout = true;
-            }
-
-            // update
-            Map<String, Object> properties = new HashMap<String, Object>();
-            properties.put(PropertyIds.NAME, NAME2);
-
-            ObjectId newId = workDoc.updateProperties(properties, false);
-            Document doc2 = (Document) session.getObject(newId, SELECT_ALL_NO_CACHE_OC);
-
-            addResult(checkObject(session, doc2, getAllProperties(doc2), "Updated document compliance"));
-
-            f = createResult(FAILURE, "Document name doesn't match updated value!");
-            addResult(assertEquals(NAME2, doc2.getName(), null, f));
-
-            // update nothing
-            try {
-                properties = new HashMap<String, Object>();
-                doc2.updateProperties(properties, false);
-            } catch (Exception e) {
-                addResult(createResult(WARNING,
-                        "updateProperties without property changes returned an error: " + e.getMessage(), e, false));
-            }
-
-            // delete
-            if (!workDoc.getId().equals(doc2.getId())) {
-                deleteObject(doc2);
-            }
-
-            // cancel a possible check out
-            if (checkedout) {
-                workDoc.cancelCheckOut();
-            }
-
-            if (!doc1.getId().equals(doc2.getId())) {
-                if (exists(doc1)) {
-                    deleteObject(doc1);
-                }
-            }
+            // folder test
+            updateFolder(session, testFolder);
         } finally {
             // clean up
             deleteTestFolder();
         }
+    }
+
+    private void updateDocument(Session session, Folder testFolder) {
+        CmisTestResult f;
+
+        // create document
+        Document doc1 = createDocument(session, testFolder, DOC_NAME1, "rename me!");
+        Document workDoc = doc1;
+
+        f = createResult(FAILURE, "Document name doesn't match the given name!");
+        addResult(assertEquals(DOC_NAME1, doc1.getName(), null, f));
+
+        // test if check out is required
+        boolean checkedout = false;
+        DocumentTypeDefinition type = (DocumentTypeDefinition) doc1.getType();
+        PropertyDefinition<?> namePropDef = type.getPropertyDefinitions().get(PropertyIds.NAME);
+        if (namePropDef.getUpdatability() == Updatability.WHENCHECKEDOUT
+                || (!doc1.getAllowableActions().getAllowableActions().contains(Action.CAN_UPDATE_PROPERTIES) && Boolean.TRUE
+                        .equals(type.isVersionable()))) {
+            workDoc = (Document) session.getObject(doc1.checkOut(), SELECT_ALL_NO_CACHE_OC);
+            checkedout = true;
+        }
+
+        // update
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(PropertyIds.NAME, DOC_NAME2);
+
+        ObjectId newId = workDoc.updateProperties(properties, false);
+        Document doc2 = (Document) session.getObject(newId, SELECT_ALL_NO_CACHE_OC);
+
+        addResult(checkObject(session, doc2, getAllProperties(doc2), "Updated document compliance"));
+
+        f = createResult(FAILURE, "Document name doesn't match updated value!");
+        addResult(assertEquals(DOC_NAME2, doc2.getName(), null, f));
+
+        // update nothing
+        try {
+            properties = new HashMap<String, Object>();
+            doc2.updateProperties(properties, false);
+        } catch (Exception e) {
+            addResult(createResult(WARNING,
+                    "updateProperties without property changes returned an error: " + e.getMessage(), e, false));
+        }
+
+        // delete
+        if (!workDoc.getId().equals(doc2.getId())) {
+            deleteObject(doc2);
+        }
+
+        // cancel a possible check out
+        if (checkedout) {
+            workDoc.cancelCheckOut();
+        }
+
+        if (!doc1.getId().equals(doc2.getId())) {
+            if (exists(doc1)) {
+                deleteObject(doc1);
+            }
+        }
+    }
+
+    private void updateFolder(Session session, Folder testFolder) {
+        CmisTestResult f;
+
+        Folder folder = createFolder(session, testFolder, FOLDER_NAME1);
+
+        f = createResult(FAILURE, "Folder name doesn't match the given name!");
+        addResult(assertEquals(FOLDER_NAME1, folder.getName(), null, f));
+
+        // update
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(PropertyIds.NAME, FOLDER_NAME2);
+
+        ObjectId newId = folder.updateProperties(properties, false);
+
+        f = createResult(WARNING, "Folder id changed after name update! The folder id should never change!");
+        addResult(assertEquals(folder.getId(), newId.getId(), null, f));
+
+        // get the new folder object and check the new name
+        folder.refresh();
+
+        f = createResult(FAILURE, "Folder name doesn't match updated value!");
+        addResult(assertEquals(FOLDER_NAME2, folder.getName(), null, f));
+
+        deleteObject(folder);
     }
 }
