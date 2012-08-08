d54b8a9ca59136702c03c281d2d0d8bba5ee9c95
Florian Müller <fmui@apache.org>
2012-08-08 12:34:48 +0000

    CMIS-517: Local binding: use sort applyAcl() directly
    git-svn-id: https://svn.apache.org/repos/asf/chemistry/opencmis/trunk@1370748 13f79535-47bb-0310-9956-ffa450edef68

--- chemistry-opencmis-client/chemistry-opencmis-client-bindings/src/main/java/org/apache/chemistry/opencmis/client/bindings/spi/local/AclServiceImpl.java
+++ chemistry-opencmis-client/chemistry-opencmis-client-bindings/src/main/java/org/apache/chemistry/opencmis/client/bindings/spi/local/AclServiceImpl.java
@@ -20,15 +20,10 @@
  */
 package org.apache.chemistry.opencmis.client.bindings.spi.local;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.chemistry.opencmis.client.bindings.spi.BindingSession;
-import org.apache.chemistry.opencmis.commons.data.Ace;
 import org.apache.chemistry.opencmis.commons.data.Acl;
 import org.apache.chemistry.opencmis.commons.data.ExtensionsData;
 import org.apache.chemistry.opencmis.commons.enums.AclPropagation;
-import org.apache.chemistry.opencmis.commons.impl.dataobjects.AccessControlListImpl;
 import org.apache.chemistry.opencmis.commons.server.CmisService;
 import org.apache.chemistry.opencmis.commons.server.CmisServiceFactory;
 import org.apache.chemistry.opencmis.commons.spi.AclService;
@@ -69,19 +64,7 @@ public class AclServiceImpl extends AbstractLocalService implements AclService, 
         CmisService service = getService(repositoryId);
 
         try {
-            Acl currentAcl = service.getAcl(repositoryId, objectId, false, null);
-
-            List<Ace> removeAces = new ArrayList<Ace>();
-            if (currentAcl.getAces() != null) {
-                for (Ace ace : currentAcl.getAces()) {
-                    if (ace.isDirect()) {
-                        removeAces.add(ace);
-                    }
-                }
-            }
-
-            return service.applyAcl(repositoryId, objectId, aces, new AccessControlListImpl(removeAces),
-                    AclPropagation.OBJECTONLY, null);
+            return service.applyAcl(repositoryId, objectId, aces, AclPropagation.OBJECTONLY);
         } finally {
             service.close();
         }
