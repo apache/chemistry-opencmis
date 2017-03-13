ebd4d62e3ec26b6a01e6f9e45f99b2839f29810e
Jens Hübel <jens@apache.org>
2017-03-13 09:46:35 +0000

    InMemory: fix a small typo (CMIS-1014)
    git-svn-id: https://svn.apache.org/repos/asf/chemistry/opencmis/trunk@1786650 13f79535-47bb-0310-9956-ffa450edef68

--- chemistry-opencmis-server/chemistry-opencmis-server-inmemory/src/main/java/org/apache/chemistry/opencmis/inmemory/NameValidator.java
+++ chemistry-opencmis-server/chemistry-opencmis-server-inmemory/src/main/java/org/apache/chemistry/opencmis/inmemory/NameValidator.java
@@ -73,7 +73,7 @@ public final class NameValidator {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '\\' || c == '/' || c == '\"' || c == ':' || c == '*' || c == '?' || c == '<' || c == '>'
-                    && c == '|') {
+                    || c == '|') {
                 return false;
             }
         }
